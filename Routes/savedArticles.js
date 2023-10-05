43c1e9abf11dece1614ba75d6916a2686ec2688a
Isaac Kojo Mensah Amfoh <isaacamfoh001@gmail.com>
2023-10-05 19:47:08 +0000

    Verify each user before they save an article

--- Routes/savedArticles.js
+++ Routes/savedArticles.js
@@ -4,9 +4,10 @@ const {
   saveArticle,
   getSavedArticles,
 } = require("../Controllers/savedArticles");
+const { verifyToken } = require("../utils/verifyToken");
 
 // Route to save an article for a user
-router.post("/save-article/:articleId", saveArticle);
+router.post("/save-article/:articleId", verifyToken, saveArticle);
 
 // Route to get saved articles for a user
 router.get("/saved-articles", getSavedArticles);
