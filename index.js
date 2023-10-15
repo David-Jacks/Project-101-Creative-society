88b9a68fe0ea24db061154e24e8df96157df01aa
Isaac Kojo Mensah Amfoh <isaacamfoh001@gmail.com>
2023-10-15 13:56:26 +0000

    Allow requests from any origin

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json({ limit: "100mb" }));
 
 // CORS configuration to allow any origin
 const corsOptions = {
-  origin: "https://lugblog.netlify.app/", // Allow requests from any origin
+  origin: "*", // Allow requests from any origin
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true, // Enable credentials (cookies, authorization headers, etc.)
   allowedHeaders: "*", // Allow any headers
