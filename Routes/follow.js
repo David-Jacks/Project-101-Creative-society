0c6b1ff5d2aae161309f03f997142735f169a3e8
Isaac Kojo Mensah Amfoh <isaacamfoh001@gmail.com>
2023-10-21 01:09:51 +0000

    Modified the params to just id

--- Routes/follow.js
+++ Routes/follow.js
@@ -9,15 +9,15 @@ const {
 const { verifyToken } = require("../utils/verifyToken");
 
 // Follow a user
-router.post("/follow/:userId", verifyToken, followUser);
+router.post("/follow/:id", verifyToken, followUser);
 
 // Unfollow a user
-router.post("/unfollow/:userId", verifyToken, unfollowUser);
+router.post("/unfollow/:id", verifyToken, unfollowUser);
 
 // Get a user's followers
-router.get("/followers/:userId", verifyToken, getFollowers);
+router.get("/followers/:id", verifyToken, getFollowers);
 
 // Get users followed by a user
-router.get("/following/:userId", verifyToken, getFollowing);
+router.get("/following/:id", verifyToken, getFollowing);
 
 module.exports = router;
