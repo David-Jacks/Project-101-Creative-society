0618760441413e54a447f65078aaa409dbbdc8a0
Isaac Kojo Mensah Amfoh <isaacamfoh001@gmail.com>
2023-10-23 01:55:57 +0000

    Modified the savedArticles controller

--- Controllers/savedArticles.js
+++ Controllers/savedArticles.js
@@ -3,33 +3,27 @@ const router = express.Router();
 const { User } = require("../Models/User"); // Import User model
 const { Post } = require("../Models/Post"); // Import Post model
 
+// Function to save an article for a user
 const saveArticle = async (req, res, next) => {
   try {
     const userId = req.user.id;
+    const articleId = req.params.articleId;
+
+    // Retrieve the article by ID
+    const article = await Post.findById(articleId);
+
+    if (!article) {
+      return res.status(404).send("Article not found.");
+    }
 
     // Add the entire article object to the user's savedArticles array
     await User.findByIdAndUpdate(userId, { $push: { savedArticles: article } });
 
     res.status(200).send("Article saved successfully.");
   } catch (error) {
-    console.log("This is the Error: ", error);
     next(error);
   }
 };
-
-// Function to get saved articles for a user
-// const getSavedArticles = async (req, res, next) => {
-//   try {
-//     const userId = req.user._id;
-
-//     // Find the user by ID and populate the savedArticles array with article details
-//     const user = await User.findById(userId).populate("savedArticles");
-
-//     res.status(200).json(user.savedArticles);
-//   } catch (error) {
-//     next(error);
-//   }
-// };
 
 // Function to get saved articles for a user
 const getSavedArticles = async (req, res, next) => {
@@ -37,16 +31,30 @@ const getSavedArticles = async (req, res, next) => {
     const userId = req.user.id;
 
     // Find the user by ID and populate the savedArticles array with article details
-    const user = await User.findById(userId).populate({
-      path: "savedArticles",
-      model: "User", // Assuming your model for articles is named "Article"
-    });
+    const user = await User.findById(userId).populate("savedArticles");
 
     res.status(200).json(user.savedArticles);
   } catch (error) {
     next(error);
   }
 };
+
+// Function to get saved articles for a user
+// const getSavedArticles = async (req, res, next) => {
+//   try {
+//     const userId = req.user.id;
+
+//     // Find the user by ID and populate the savedArticles array with article details
+//     const user = await User.findById(userId).populate({
+//       path: "savedArticles",
+//       model: "User", // Assuming your model for articles is named "Article"
+//     });
+
+//     res.status(200).json(user.savedArticles);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
 
 module.exports.getSavedArticles = getSavedArticles;
 module.exports.saveArticle = saveArticle;
