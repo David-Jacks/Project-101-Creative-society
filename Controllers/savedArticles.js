217411be4951671835fd5ca9eb8976314bf4d8db
Isaac Kojo Mensah Amfoh <isaacamfoh001@gmail.com>
2023-10-23 01:46:54 +0000

    More changes

--- Controllers/savedArticles.js
+++ Controllers/savedArticles.js
@@ -3,36 +3,19 @@ const router = express.Router();
 const { User } = require("../Models/User"); // Import User model
 const { Post } = require("../Models/Post"); // Import Post model
 
-// Function to save an article for a user
-// const saveArticle = async (req, res, next) => {
-//   try {
-//     const userId = req.user.id;
-//     const articleId = req.params.articleId;
-//     console.log("req.user object : ", req.user);
-//     console.log("UserId: ", userId);
-//     console.log("articleId: ", articleId);
+const saveArticle = async (req, res, next) => {
+  try {
+    const userId = req.user.id;
 
-//     // Retrieve the article by ID
-//     const article = await Post.findById(articleId);
-//     console.log("The article: ", article);
+    // Add the entire article object to the user's savedArticles array
+    await User.findByIdAndUpdate(userId, { $push: { savedArticles: article } });
 
-//     if (!article) {
-//       return res.status(404).send("Article not found.");
-//     }
-//     console.log("The article was found...");
-
-//     // Add the entire article object to the user's savedArticles array
-//     await User.findByIdAndUpdate(userId, { $push: { savedArticles: article } });
-//     console.log("saved user object: ", User);
-
-//     console.log("It worked......");
-
-//     res.status(200).send("Article saved successfully.");
-//   } catch (error) {
-//     console.log("This is the Error: ", error);
-//     next(error);
-//   }
-// };
+    res.status(200).send("Article saved successfully.");
+  } catch (error) {
+    console.log("This is the Error: ", error);
+    next(error);
+  }
+};
 
 // Function to get saved articles for a user
 // const getSavedArticles = async (req, res, next) => {
@@ -47,24 +30,6 @@ const { Post } = require("../Models/Post"); // Import Post model
 //     next(error);
 //   }
 // };
-
-// Function to save an article for a user
-const saveArticle = async (req, res, next) => {
-  try {
-    const userId = req.user.id;
-    const articleId = req.params.articleId;
-
-    // Add the article ID to the user's savedArticles array
-    await User.findByIdAndUpdate(userId, {
-      $addToSet: { savedArticles: articleId },
-    });
-
-    res.status(200).send("Article saved successfully.");
-  } catch (error) {
-    console.log("This is the Error: ", error);
-    next(error);
-  }
-};
 
 // Function to get saved articles for a user
 const getSavedArticles = async (req, res, next) => {
